f59bb88665ed92230a1b341bb49413ce5b1c19d4
Anthony Ciezadlik <aciezadl@f1br3s7.clusters.42paris.fr>
2025-05-26 14:46:35 +0200

    add_cgi

---
+++ srcs/cgi-bin/redirect.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "Status: 302 Found"
+echo "Location: https://www.youtube.com/"
+echo "Content-Type: text/html"
+echo ""
+
+echo "<html><body>Redirection vers <a href='https://www.youtube.com/'>Youtube</a>...</body></html>"
