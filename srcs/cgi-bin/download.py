5854348948ae9f6996d3772e80837810380e76cc
Alan De franceschi <ade-fran@f2r5s10.clusters.42paris.fr>
2025-07-15 18:11:31 +0200

    up

--- srcs/cgi-bin/download.py
+++ srcs/cgi-bin/download.py
@@ -4,19 +4,24 @@ import sys
 def main():
 
     filepath = "./Makefile"
+
     if not os.path.isfile(filepath):
         print("Status: 404 Not Found")
         print("Content-Type: text/plain\r\n")
         print("Fichier non trouvé.")
         return
-    sys.stdout.write("Content-Type: application/octet-stream\r\n")
-    sys.stdout.write(f"Content-Disposition: attachment; filename=\"{os.path.basename(filepath)}\"\r\n")
-    sys.stdout.write("Content-Length: {}\r\n".format(os.path.getsize(filepath)))
-    sys.stdout.write("\r\n")
-    sys.stdout.flush()
+    
+    headers = (
+        "Content-Type: application/octet-stream\r\n"
+        f"Content-Disposition: attachment; filename=\"{os.path.basename(filepath)}\"\r\n"
+        f"Content-Length: {os.path.getsize(filepath)}\r\n"
+        "\r\n"
+    )
+
+    sys.stdout.buffer.write(headers.encode())
     with open(filepath, "rb") as f:
         sys.stdout.buffer.write(f.read())
-        sys.stdout.buffer.flush()
+    sys.stdout.buffer.flush()
 
 if __name__ == "__main__":
     main()
